3db751a91b87677f71fda31d458518e56ffd5f34
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-01 12:39:04 +0200

    add inputs

--- src/pages/user/church/crud/create/field.tsx
+++ src/pages/user/church/crud/create/field.tsx
@@ -1,11 +1,15 @@
 import { Save } from "@mui/icons-material"
 import { Button, TextField } from "@mui/material"
+import { useState } from "react"
+import { ChurchInput } from "../../../../../types/chuchInput"
+import { ChurchType } from "../../../../../enum/churchType"
 
 export const Field=()=>{
+    const [church,setChurch]=useState<ChurchInput>({location:'',name:'',type:ChurchType.FIELD});
     return (
         <>
-        <TextField variant="standard" label='Field name' fullWidth  className="mb-3"/>
-        <TextField variant="standard" label='Field Location' fullWidth className="mb-3"/>
+        <TextField variant="standard" value={church.name} onChange={(e)=>setChurch({...church,name:e.target.value})} label='Field name' fullWidth  className="mb-3"/>
+        <TextField variant="standard" value={church.location} onChange={(e)=>setChurch({...church,location:e.target.value})} label='Field Location' fullWidth className="mb-3"/>
         <div className="modal-footer">
             <Button variant="contained"><Save/></Button>
         </div>
