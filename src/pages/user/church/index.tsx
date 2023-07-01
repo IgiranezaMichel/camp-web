869b3b42fecc58be32653741a7bd8c61e5f53a17
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-01 12:19:27 +0200

    add church

--- src/pages/user/church/index.tsx
+++ src/pages/user/church/index.tsx
@@ -1,6 +1,22 @@
+import { Button, Dialog } from "@mui/material"
+import { SideBarNavigation } from "../../../component/navigation"
+import { Url } from "../../../url/url"
+import { AddChurch } from "./crud/create"
+import { useState } from "react"
+import { Close } from "@mui/icons-material"
+
 export const Church=()=>{
+    const [openAddChurchDialog,setOpenAddChurchDialog]=useState(false);
     return(
-        <>
-        </>
+        <SideBarNavigation activeBar="/admin/church" items={Url}>
+            <div>
+                <Button className=""  onClick={()=>setOpenAddChurchDialog(true)} variant="contained">Add New Church</Button>
+            </div>
+            <Dialog open={openAddChurchDialog} PaperProps={{className:'col-md-6'}}>
+                <AddChurch id="">
+                    <div>Add New Church <Close className="float-end" onClick={()=>setOpenAddChurchDialog(false)}/></div>
+                </AddChurch>
+            </Dialog>
+        </SideBarNavigation>
     )
 }
