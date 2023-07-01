e0ced7e84e939abb1d48b04f718e1f58cb4c6669
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-01 12:30:30 +0200

    add church

--- src/pages/user/church/crud/create/index.tsx
+++ src/pages/user/church/crud/create/index.tsx
@@ -1,15 +1,35 @@
-import { NativeSelect } from "@mui/material"
-import { ReactNode } from "react"
+import { TabContext, TabList, TabPanel } from "@mui/lab"
+import { Box, Tab } from "@mui/material"
+import { ReactNode, useState } from "react"
+import { Field } from "./field";
+import { District } from "./district";
+import { ChurchZone } from "./church";
 
 export const AddChurch=(props:{children:ReactNode,id:string})=>{
+    const [value, setValue] = useState('1');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        event;
+        setValue(newValue);
+    };
     return(
         <>
             <div className="p-3">{props.children}</div>
             <div className="p-3">
-                Add new 
-                <NativeSelect fullWidth className="mb-2">
-                    <option value="">Select type of church you want to create</option>
-                </NativeSelect>
+            <Box sx={{ width: '100%', typography: 'body1' }}>
+                <TabContext value={value}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <TabList onChange={handleChange}>
+                            <Tab label="Field" value="1" />
+                            <Tab label="District" value="2" />
+                            <Tab label="Church" value="2" />
+                        </TabList>
+                    </Box>
+                    <TabPanel value="1"><Field/></TabPanel>
+                    <TabPanel value="2"><District/></TabPanel>
+                    <TabPanel value="3"><ChurchZone/></TabPanel>
+                </TabContext>
+            </Box>
             </div>
         </>
     )
