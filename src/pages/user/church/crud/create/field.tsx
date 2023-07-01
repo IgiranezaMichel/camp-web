f9251c6cd0599e274eb480f3ed994a2d0d2757fd
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-01 12:30:23 +0200

    add field

--- src/pages/user/church/crud/create/field.tsx
+++ src/pages/user/church/crud/create/field.tsx
@@ -1,7 +1,14 @@
+import { Save } from "@mui/icons-material"
+import { Button, TextField } from "@mui/material"
+
 export const Field=()=>{
     return (
         <>
-        Field
+        <TextField variant="standard" label='Field name' fullWidth  className="mb-3"/>
+        <TextField variant="standard" label='Field Location' fullWidth className="mb-3"/>
+        <div className="modal-footer">
+            <Button variant="contained"><Save/></Button>
+        </div>
         </>
     )
 }
