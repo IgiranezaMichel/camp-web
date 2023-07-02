417edd8038f592918a9da8c4fd0023634640dfa2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-02 12:52:02 +0200

    add FIeld input

--- src/pages/user/church/crud/create/field.tsx
+++ src/pages/user/church/crud/create/field.tsx
@@ -3,16 +3,32 @@ import { Button, TextField } from "@mui/material"
 import { useState } from "react"
 import { ChurchInput } from "../../../../../types/chuchInput"
 import { ChurchType } from "../../../../../enum/churchType"
+import { useSaveOrUpdateChurch } from "../../../../../controller/church/mutation"
+import { ToastContainer, toast } from "react-toastify"
 
 export const Field=()=>{
     const [church,setChurch]=useState<ChurchInput>({location:'',name:'',type:ChurchType.FIELD});
+    const {saveChurch}=useSaveOrUpdateChurch(church);
+    const saveChurchHandler=()=>{
+        saveChurch().then(
+            (data)=>{
+            const result = data.data.saveOrUpdateChurch;
+            const splitting = String(result).substring(1, String(result).lastIndexOf(',')).split(',');
+            const code = Number(splitting[0].split(' ')[0])
+            const responseText = splitting[1];
+            code == 200 ? toast.success(responseText) : toast.error(responseText);
+            }
+        )
+        .catch(err=>console.log(err))
+    }
     return (
         <>
         <TextField variant="standard" value={church.name} onChange={(e)=>setChurch({...church,name:e.target.value})} label='Field name' fullWidth  className="mb-3"/>
         <TextField variant="standard" value={church.location} onChange={(e)=>setChurch({...church,location:e.target.value})} label='Field Location' fullWidth className="mb-3"/>
         <div className="modal-footer">
-            <Button variant="contained"><Save/></Button>
+            <Button variant="contained" onChange={()=>saveChurchHandler()}><Save/></Button>
         </div>
+        <ToastContainer/>
         </>
     )
 }
