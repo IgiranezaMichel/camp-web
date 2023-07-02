e450756848db0564366e10618d3b266969041a50
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-02 12:51:50 +0200

    add District input

--- src/pages/user/church/crud/create/district.tsx
+++ src/pages/user/church/crud/create/district.tsx
@@ -1,7 +1,34 @@
+import { useState } from "react";
+import { useSaveOrUpdateChurch } from "../../../../../controller/church/mutation";
+import { ChurchType } from "../../../../../enum/churchType";
+import { ChurchInput } from "../../../../../types/chuchInput";
+import { ToastContainer, toast } from "react-toastify";
+import { Button, TextField } from "@mui/material";
+import { Save } from "@mui/icons-material";
+
 export const District=()=>{
-    return(
+    const [church,setChurch]=useState<ChurchInput>({location:'',name:'',type:ChurchType.DISTRICT});
+    const {saveChurch}=useSaveOrUpdateChurch(church);
+    const saveChurchHandler=()=>{
+        saveChurch().then(
+            (data)=>{
+            const result = data.data.saveOrUpdateChurch;
+            const splitting = String(result).substring(1, String(result).lastIndexOf(',')).split(',');
+            const code = Number(splitting[0].split(' ')[0])
+            const responseText = splitting[1];
+            code == 200 ? toast.success(responseText) : toast.error(responseText);
+            }
+        )
+        .catch(err=>console.log(err))
+    }
+    return (
         <>
-        District
+        <TextField variant="standard" value={church.name} onChange={(e)=>setChurch({...church,name:e.target.value})} label='Field name' fullWidth  className="mb-3"/>
+        <TextField variant="standard" value={church.location} onChange={(e)=>setChurch({...church,location:e.target.value})} label='Field Location' fullWidth className="mb-3"/>
+        <div className="modal-footer">
+            <Button variant="contained" onChange={()=>saveChurchHandler()}><Save/></Button>
+        </div>
+        <ToastContainer/>
         </>
     )
 }
