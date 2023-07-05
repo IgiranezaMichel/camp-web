a52154694f5a3aa9a5bc9b1542e44cd033af42ed
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2023-07-05 08:33:22 +0200

    add districts

--- src/pages/user/church/crud/index.tsx
+++ src/pages/user/church/crud/index.tsx
@@ -1,20 +1,32 @@
-import { Card, CircularProgress } from "@mui/material";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Avatar, Card, Chip, CircularProgress } from "@mui/material";
 import { useChurchContext } from "../../../../contexts/churchContext"
+import { Add, Close, LocationOn } from "@mui/icons-material";
+import { AddChurchLeader } from "./create/leader";
+import { useState } from "react";
 
-export const DisplayChurch=()=>{
-    const{content}=useChurchContext();
-    console.log(content)
-    return(
+export const DisplayChurch = () => {
+    const { content } = useChurchContext();
+    const [open,setOpen]=useState(false);
+    return (
         <>
-        {!content.responseReady&&<div className="text-center p-5"><CircularProgress/></div>}
-        {content&&content.responseReady&&content.responseContent&&content.responseContent!=undefined&&content.responseContent.length!=0&&
-        content.responseContent.map((data,index:number)=>{
-            return <Card className="mt-3 p-1 rounded-0 border border-black">
-                <h3>{data.name}</h3>
-                <div>{data.location}</div>
-            </Card>
-        })
-        }
+            {!content.responseReady && <div className="text-center p-5"><CircularProgress /></div>}
+            {content && content.responseReady && content.responseContent && content.responseContent != undefined && content.responseContent.length != 0 &&
+                content.responseContent.map((data: any, index: number) => {
+                    return <Card className="mt-3 p-3 rounded-0 border border-black" key={index}>
+                        <h3 className="fw-bold">{data.name}</h3>
+                        <div className="mb-2"><LocationOn/> {data.location}</div>
+                        <div className="float-end"><Chip label={<span className="fw-bold">Total Districts <span className="badge bg-black">{data.churchList.length}</span></span>} /><b></b></div>
+                        <Chip onClick={()=>setOpen(true)} avatar={<Avatar className="bg-black"><Add className="text-white"/></Avatar>} className="bg-primary text-white fw-bold" label='Add Leader' />
+                        <Chip avatar={<Avatar />} className="bg-dark mx-1 text-white fw-bold" label='View Leader' />
+                        <div className="modal-footer">
+                        </div>
+                    </Card>
+                })
+            }
+            <AddChurchLeader open={open}>
+                <div className="p-3 mb-2 fw-bold">Add Church leader <Close className="float-end" onClick={()=>setOpen(false)}/></div>
+            </AddChurchLeader>
         </>
     )
 }
