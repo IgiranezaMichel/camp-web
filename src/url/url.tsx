6155836c7da8bf5ceb2652115af180f1182cbc28
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 16:06:20 +0200

    add NAvigationItem

---
+++ src/url/url.tsx
@@ -0,0 +1,11 @@
+import { Book, Dashboard, FileOpenSharp, LocationOn, People, Stairs } from "@mui/icons-material";
+import { NavigationItem } from "../types/navigationItem";
+
+export const Url:NavigationItem[]=[
+    {icon:<Dashboard/>,title:'Home',url:'/admin'},
+    {icon:<Stairs/>,title:'Levels',url:'/admin/level'},
+    {icon:<Book/>,title:'Books',url:'/admin/level'},
+    {icon:<FileOpenSharp/>,title:'Exams',url:'/admin/exams'},
+    {icon:<LocationOn/>,title:'Camps',url:'/admin/camps'},
+    {icon:<People/>,title:'Camp Leaders',url:'/admin/leader'},
+  ]
