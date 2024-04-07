000e0d65485f6ee3adb8d64353196070859d23d0
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 21:42:14 +0200

    add books url

--- src/url/url.tsx
+++ src/url/url.tsx
@@ -4,7 +4,7 @@ import { NavigationItem } from "../types/navigationItem";
 export const Url:NavigationItem[]=[
     {icon:<Dashboard/>,title:'Home',url:'/admin'},
     {icon:<Stairs/>,title:'Levels',url:'/admin/level'},
-    {icon:<Book/>,title:'Books',url:'/admin/level'},
+    {icon:<Book/>,title:'Books',url:'/admin/book'},
     {icon:<FileOpenSharp/>,title:'Exams',url:'/admin/exams'},
     {icon:<LocationOn/>,title:'Camps',url:'/admin/camps'},
     {icon:<People/>,title:'Camp Leaders',url:'/admin/leader'},
