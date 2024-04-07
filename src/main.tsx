46bc59bf572b82e03203a1dec0f0f959d3be7dc4
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 21:37:29 +0200

    add toastify css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap.js'
+import 'react-toastify/dist/ReactToastify.css';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import App from './App';
 const client = new ApolloClient({
