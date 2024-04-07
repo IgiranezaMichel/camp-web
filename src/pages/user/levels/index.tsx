095aa544db6ef79a22e780c13977edb3291e2245
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 21:40:22 +0200

    add DisplayLevels

--- src/pages/user/levels/index.tsx
+++ src/pages/user/levels/index.tsx
@@ -1,11 +1,12 @@
-import { Folder } from "@mui/icons-material"
-import { Admin } from ".."
-import { Box, Button } from "@mui/material"
-import Tab from '@mui/material/Tab';
+import { Folder } from "@mui/icons-material";
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { Box, Button } from "@mui/material";
+import Tab from '@mui/material/Tab';
 import { useState } from "react";
+import { Admin } from "../../../component/admin";
+import { DisplayLevels } from "./crud";
 export const LevelUi = () => {
     const [value, setValue] = useState('1');
 
@@ -16,10 +17,14 @@ export const LevelUi = () => {
     return (
         <Admin>
             <div className="card rounded-0">
-                <img src="download.jpg" alt="" style={{ filter: 'blur(4px)' }} />
+                <img src="../download.jpg" alt="" style={{ filter: 'blur(4px)' }} />
                 <div className="card-img-overlay  d-flex justify-content-center align-items-center">
-                    <Button variant="contained" className="display-5" data-bs-toggle="modal"
-                        data-bs-target="#id"><Folder className="fs-1" /> Add New Level</Button>
+                    <div>
+                    <h6>Do you want to create </h6>
+                    <h6>New Adventist </h6>
+                    <h6>youth Level?</h6>
+                    <Button variant="contained" className="display-5 fw-bold border border-1 border-white" data-bs-toggle="modal" data-bs-target="#id"><Folder className="fs-1" /> Add New Level</Button>
+                    </div>
                 </div>
             </div>
             <Box sx={{ width: '100%', typography: 'body1' }}>
@@ -30,9 +35,8 @@ export const LevelUi = () => {
                             <Tab label="Users" value="2" />
                         </TabList>
                     </Box>
-                    <TabPanel value="1">Item One</TabPanel>
+                    <TabPanel value="1"><DisplayLevels/></TabPanel>
                     <TabPanel value="2">Item Two</TabPanel>
-                    <TabPanel value="3">Item Three</TabPanel>
                 </TabContext>
             </Box>
         </Admin>
