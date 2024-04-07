db79dbf26310c546017e06547d30b1f8603e5d5c
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 16:04:28 +0200

    add appolo client

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import App from './App';
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache(),
+});
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+)
