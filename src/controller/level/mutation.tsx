66a37eb7b70de3ff2e769a26428e981e80c53e7a
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-07 21:38:18 +0200

    add userDeleteLevel

---
+++ src/controller/level/mutation.tsx
@@ -0,0 +1,17 @@
+import { useMutation } from "@apollo/client"
+import { LevelInput } from "../../types/levelInput"
+import { CREATE_LEVELS, DELETE_LEVELS } from "../../graphql/level/mutation"
+export const useSaveOrUpdateLevel=(level:LevelInput)=>{
+const [save]=useMutation(CREATE_LEVELS);
+const saveLevel=async()=>{
+return await save({variables:{levelInput:level}})
+}
+return {saveLevel}
+}
+export const useDeleteLevel=(id:string)=>{
+    const [deleteLevel]=useMutation(DELETE_LEVELS);
+    const deleteHandler=async()=>{
+    return await deleteLevel({variables:{id:id}})
+    }
+    return {deleteHandler}
+    }
