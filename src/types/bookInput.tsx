3bea1fd66e3b5a83f3ad013133a364d923c1f7f7
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-08 16:55:06 +0200

    add BookInput

---
+++ src/types/bookInput.tsx
@@ -0,0 +1,11 @@
+export type BookInput = {
+    id: string
+    name: string
+    base64File: string
+    levelId: string
+    base64Cover: string
+    author: string
+    publicationDate: string
+    publisher: string
+    serialNumber:string
+}
