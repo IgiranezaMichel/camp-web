8d4a82adcbb9db739456d3866fe416f119a9fa13
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-08 16:53:10 +0200

    add useSaveOrUpdateLevel

--- src/controller/level/mutation.tsx
+++ src/controller/level/mutation.tsx
@@ -1,17 +1,17 @@
 import { useMutation } from "@apollo/client"
 import { LevelInput } from "../../types/levelInput"
 import { CREATE_LEVELS, DELETE_LEVELS } from "../../graphql/level/mutation"
-export const useSaveOrUpdateLevel=(level:LevelInput)=>{
-const [save]=useMutation(CREATE_LEVELS);
-const saveLevel=async()=>{
-return await save({variables:{levelInput:level}})
-}
-return {saveLevel}
-}
-export const useDeleteLevel=(id:string)=>{
-    const [deleteLevel]=useMutation(DELETE_LEVELS);
-    const deleteHandler=async()=>{
-    return await deleteLevel({variables:{id:id}})
+export const useSaveOrUpdateLevel = (level: LevelInput) => {
+    const [save] = useMutation(CREATE_LEVELS);
+    const saveLevel = async () => {
+        return await save({ variables: { levelInput: level } })
     }
-    return {deleteHandler}
+    return { saveLevel }
+}
+export const useDeleteLevel = (id: string) => {
+    const [deleteLevel] = useMutation(DELETE_LEVELS);
+    const deleteHandler = async () => {
+        return await deleteLevel({ variables: { id: id } })
     }
+    return { deleteHandler }
+}
