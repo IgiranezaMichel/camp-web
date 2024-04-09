714926c2d1232bf37ca215f3e9600856c075053b
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-09 19:51:57 +0200

    add camp hooks

---
+++ src/controller/camp/mutation.tsx
@@ -0,0 +1,18 @@
+import { useMutation } from "@apollo/client";
+import { CampInput } from "../../types/campInput";
+import { CREATE_CAMP, DELETE_CAMP } from "../../graphql/camp/mutation";
+
+export const useSaveOrUpdateCamp = (campInput: CampInput) => {
+    const [save] = useMutation(CREATE_CAMP);
+    const saveCamp = async () => {
+        return await save({ variables: { campInput: campInput } })
+    }
+    return { saveCamp }
+}
+export const useDeleteCamp = (id: string) => {
+    const [deleteCamp] = useMutation(DELETE_CAMP);
+    const deleteHandler = async () => {
+        return await deleteCamp({ variables: { id: id } })
+    }
+    return { deleteHandler }
+}
