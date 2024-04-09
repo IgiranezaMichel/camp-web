2fab248035783871580bf964bb061a86141d8d53
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-09 19:44:29 +0200

    add CampInput

---
+++ src/types/campInput.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export type CampInput={
+title:string
+cost:number 
+address:string 
+location:string 
+startingDate:string 
+endingDate:string 
+description:string 
+levels:any
+content:string
+}
