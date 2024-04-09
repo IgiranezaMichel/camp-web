eb419c51b2586d22db66d39bc06c4a7201632b79
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-09 19:43:12 +0200

    add Delete Exam

---
+++ src/pages/user/exam/crud/delete.tsx
@@ -0,0 +1,9 @@
+import { BootstrapModal } from "../../../../component/modal"
+
+export const DeleteExam=()=>{
+    return (
+        <BootstrapModal id="delete" size="modal-sm">
+            <div></div>
+        </BootstrapModal>
+    )
+}
