3cd94685a0782514ddabc6766f8006b23c553e72
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-10 08:16:42 +0200

    add open dialog

--- src/pages/user/camp/index.tsx
+++ src/pages/user/camp/index.tsx
@@ -5,39 +5,53 @@ import { CreateCamp } from "./crud/create"
 import { TabContext, TabList, TabPanel } from "@mui/lab"
 import { useState } from "react"
 import { Close } from "@mui/icons-material"
+import { PageInput } from "../../../types/pageInput"
+import { ContentData } from "../../../types/contentTypes"
+import { useCamp } from "../../../controller/camp/query"
+import { CampContext } from "../../../contexts/campContext"
+import { DisplayCamp } from "./crud"
 
-export const Camp=()=>{
+export const Camp = () => {
     const [value, setValue] = useState('1');
-
+    const [openCreateDialog, setOpenCreateDialog] = useState(false);
+    const [page, setPage] = useState<PageInput>({ pageNumber: 0, pageSize: 10, sort: "timeStamp" });
     const handleChange = (event: React.SyntheticEvent, newValue: string) => {
         event;
         setValue(newValue);
     };
-    const [openCreateDialog,setOpenCreateDialog]=useState(false);
-    return(
-        <SideBarNavigation activeBar="/admin/camp" items={Url}>
-            <div className="mb-5">
-                <Button onClick={()=>setOpenCreateDialog(true)} data-bs-toggle="modal" data-bs-target="#add" variant="contained" className="rounded-0">Add new camp</Button>
-            </div>
-            <Box sx={{ width: '100%'}}>
-                <TabContext value={value}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={handleChange}>
-                            <Tab label="Recent camps" value="1" />
-                            <Tab label="Camp Applicants" value="2" />
-                            <Tab label="Camp Leader History" value="3" />
-                        </TabList>
-                    </Box>
-                    <TabPanel value="1">Item 1</TabPanel>
-                    <TabPanel value="2">Item Two</TabPanel>
-                    <TabPanel value="3">Item Three</TabPanel>
-                </TabContext>
-            </Box>
-            <Dialog TransitionComponent={Slide} open={openCreateDialog}>
-            <CreateCamp>
-                <div className="p-4 sticky-top bg-white" >Add New camp <Close className="float-end" onClick={()=>setOpenCreateDialog(false)}/></div>
-            </CreateCamp>
-            </Dialog>
-        </SideBarNavigation>
+    const { response, refetch } = useCamp(page);
+    const data: ContentData = {
+        content: response,
+        updateContent() {
+            refetch();
+        },
+    }
+    return (
+        <CampContext.Provider value={data}>
+            <SideBarNavigation activeBar="/admin/camp" items={Url}>
+                <div className="mb-5">
+                    <Button onClick={() => setOpenCreateDialog(true)} data-bs-toggle="modal" data-bs-target="#add" variant="contained" className="rounded-0">Add new camp</Button>
+                </div>
+                <Box sx={{ width: '100%' }}>
+                    <TabContext value={value}>
+                        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                            <TabList onChange={handleChange}>
+                                <Tab label="Recent camps" value="1" />
+                                <Tab label="Camp Applicants" value="2" />
+                                <Tab label="Camp Leader History" value="3" />
+                            </TabList>
+                        </Box>
+                        <TabPanel value="1"><DisplayCamp/></TabPanel>
+                        <TabPanel value="2">Item Two</TabPanel>
+                        <TabPanel value="3">Item Three</TabPanel>
+                    </TabContext>
+                </Box>
+                <Dialog TransitionComponent={Slide} open={openCreateDialog}>
+                    <CreateCamp>
+                        <div className="p-4 sticky-top bg-white" >Add New camp <Close className="float-end" onClick={() => setOpenCreateDialog(false)} /></div>
+                    </CreateCamp>
+                </Dialog>
+            </SideBarNavigation>
+        </CampContext.Provider>
     )
 }
