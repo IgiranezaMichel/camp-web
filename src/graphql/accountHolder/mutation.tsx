3238b72b815f28325eef3426325017bc67959bb2
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-11 13:06:37 +0200

    add account holder

---
+++ src/graphql/accountHolder/mutation.tsx
@@ -0,0 +1,7 @@
+import { gql } from "@apollo/client";
+
+export const CREATE_ACCOUNT_HOLDER=gql`
+mutation($accountHolderInput:AccountHolderInput){
+    saveOrUpdateAccountHolder(accountHolderInput:$accountHolderInput)
+}
+`
