c1907678419dba393d18f36e960092ecc40fa97f
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-11 21:04:26 +0200

    add query to find account holder having same role

--- src/controller/duty/query.tsx
+++ src/controller/duty/query.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@apollo/client";
 import { useEffect, useState } from "react";
-import { FIND_USER_WORK_AT_THE_SAME_CHURCH } from "../../graphql/duty/query";
+import { FINDING_ACCOUNT_HOLDER_HAVING_SAME_ROLE, FIND_USER_WORK_AT_THE_SAME_CHURCH } from "../../graphql/duty/query";
 import { ResponseType } from "../../types/response";
+import { PageInput } from "../../types/pageInput";
+import { Role } from "../../enum/Role";
 
 export const useUserHavingDutyAtTheSameChurch=(churchId:string)=>{
     const [response,setResponse]=useState<ResponseType>({responseCode:0,responseContent:[],responseReady:false});
@@ -15,6 +17,21 @@ export const useUserHavingDutyAtTheSameChurch=(churchId:string)=>{
         fetch().then(data=>setResponse({responseCode:200,responseContent:data,responseReady:true}));
     },[data]
     )
-    console.log(data)
     return{response,refetch}
     }
+    export const useFindingAccountHolderHavingSameRole=(input:PageInput,role:Role)=>{
+        console.log(input)
+        console.log(role)
+        const [response,setResponse]=useState<ResponseType>({responseCode:0,responseContent:[],responseReady:false});
+        const {data,refetch}=useQuery(FINDING_ACCOUNT_HOLDER_HAVING_SAME_ROLE,{variables:{input:input,role:role}});
+        useEffect(
+        ()=>{
+            const fetch=async()=>{
+                if(data) 
+                    return data.findAccountHolderHavingSameRole;
+            }
+            fetch().then(data=>setResponse({responseCode:200,responseContent:data,responseReady:true}));
+        },[data]
+        )
+        return{response,refetch}
+        }
