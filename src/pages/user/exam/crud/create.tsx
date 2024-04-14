ab78c8f776d7798930d8ad56bb02d43810304c2d
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-14 15:52:34 +0200

    add create axam

--- src/pages/user/exam/crud/create.tsx
+++ src/pages/user/exam/crud/create.tsx
@@ -1,9 +1,24 @@
-import { BootstrapModal } from "../../../../component/modal"
+import { Save} from "@mui/icons-material"
+import { Button, Dialog, NativeSelect, TextField, TextareaAutosize } from "@mui/material"
+import { ReactNode } from "react"
 
-export const CreateExam=()=>{
+export const CreateExam=(props:{open:boolean,children:ReactNode})=>{
     return(
-        <BootstrapModal id="delete" size="modal-sm">
-            <div></div>
-        </BootstrapModal>
+        <Dialog open={props.open} >
+            <div className="col-sm-12">{props.children}</div>
+            <div className="col-sm-11 m-auto">
+            <TextField className="mb-3" fullWidth variant="standard" label='Exam Title'/>
+            <TextareaAutosize className="col-12 mb-3" placeholder="Description" minRows={3} />
+            <NativeSelect fullWidth className="mb-3">
+                <option value="">Select Camp</option>
+            </NativeSelect>
+            <NativeSelect fullWidth className="mb-3">
+                <option value="">Select level</option>
+            </NativeSelect>
+            <div className="modal-footer mb-3">
+                <Button variant="contained"><Save/></Button>
+            </div>
+            </div>
+        </Dialog>
     )
 }
