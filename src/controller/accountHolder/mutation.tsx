e06fe124a15bc6d24c0b3278ade187ea430863e8
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-14 15:47:45 +0200

    find by email

--- src/controller/accountHolder/mutation.tsx
+++ src/controller/accountHolder/mutation.tsx
@@ -1,9 +1,8 @@
 import {useMutation } from "@apollo/client";
-import { CREATE_ACCOUNT_HOLDER, DELETE_ACCOUNT_HOLDER } from "../../graphql/accountHolder/mutation";
+import { CREATE_ACCOUNT_HOLDER, DELETE_ACCOUNT_HOLDER, FIND_ACCOUNT_HOLDER_BY_EMAIL } from "../../graphql/accountHolder/mutation";
 import { AccountHolderInput } from "../../types/accountHolderInput";
 
 export const useSaveOrUpdateAccountHolder=(accountHolder:AccountHolderInput,churchId:string)=>{
-    console.log(accountHolder)
     const [save]=useMutation(CREATE_ACCOUNT_HOLDER);
     const saveAccountHolder=async()=>{
     return await save({variables:{accountHolderInput:accountHolder,churchId:churchId}})
@@ -16,4 +15,12 @@ export const useSaveOrUpdateAccountHolder=(accountHolder:AccountHolderInput,chur
         return await deleteAccountHolder({variables:{id:id}})
         }
         return {deleteHandler}
+        }
+
+export const useFindAccountHolderByEmail=(email:string)=>{
+        const [findAccountHolder]=useMutation(FIND_ACCOUNT_HOLDER_BY_EMAIL);
+        const findAccountHolderHandler=async()=>{
+        return await findAccountHolder({variables:{email:email}})
+        }
+        return {findAccountHolderHandler}
         }
