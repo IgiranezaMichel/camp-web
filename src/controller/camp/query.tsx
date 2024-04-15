be7f2a1426d5fddea8fad03a27c79cb920c83f59
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-15 21:03:02 +0200

    check active camps

--- src/controller/camp/query.tsx
+++ src/controller/camp/query.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react"
 import { ResponseType } from "../../types/response"
 import { useQuery } from "@apollo/client";
 import { PageInput } from "../../types/pageInput";
-import { ACTIVE_CAMP, CAMP_PAGE } from "../../graphql/camp/query";
+import { ACTIVE_CAMP, CAMP_PAGE, INACTIVE_CAMP } from "../../graphql/camp/query";
 
 export const useCamp=(input:PageInput)=>{
 const [response,setResponse]=useState<ResponseType>({responseCode:0,responseContent:[],responseReady:false});
@@ -36,4 +36,21 @@ export const useActiveCamp = (page: PageInput) => {
     )
     return{response,refetch}
   };
-  
+  export const useInactiveCamp = (page: PageInput) => {
+    const [response, setResponse] = useState<ResponseType>({
+      responseContent: [],
+      responseReady: false,
+      responseCode:0
+    });
+    const {data,refetch}=useQuery(INACTIVE_CAMP,{variables:{page:page}});
+    useEffect(
+    ()=>{
+        const fetch=async()=>{
+            if(data) 
+                return data.inactiveCamp;
+        }
+        fetch().then(data=>{setResponse({responseCode:200,responseContent:data,responseReady:true})});
+    },[data]
+    )
+    return{response,refetch}
+  };
