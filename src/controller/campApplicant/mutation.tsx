b1965ecd6d3637fc50b5959afa20614a174d9427
Igiraneza Michel <michel.igiraneza@auca.ac.rw>
2024-04-17 17:34:44 +0200

    add update camp applicant status

--- src/controller/campApplicant/mutation.tsx
+++ src/controller/campApplicant/mutation.tsx
@@ -1,6 +1,7 @@
 import { useMutation } from "@apollo/client";
 import { CampApplicantInput } from "../../types/campApplicationInput"
-import { CREATE_CAMP_APPLICATION } from "../../graphql/campApplicant/mutation";
+import { CREATE_CAMP_APPLICATION, UPDATE_CAMP_APPLICANT_STATUS } from "../../graphql/campApplicant/mutation";
+import { CampApplicantStatus } from "../../enum/campApplicationStatus";
 
 export const useCampApplication=(campApplicantInput:CampApplicantInput)=>{
     const [save] = useMutation(CREATE_CAMP_APPLICATION);
@@ -8,4 +9,11 @@ export const useCampApplication=(campApplicantInput:CampApplicantInput)=>{
         return await save({ variables: { campApplicantInput: campApplicantInput } })
     }
     return { saveCampApplication }
+}
+export const useUpdateCampApplicantStatus=(campApplicantId:string,status:CampApplicantStatus,comment:string)=>{
+    const [save] = useMutation(UPDATE_CAMP_APPLICANT_STATUS);
+    const updateCampApplicationStatus = async () => {
+        return await save({ variables: { campApplicantId:campApplicantId,status:status,comment:comment } })
+    }
+    return { updateCampApplicationStatus }
 }
