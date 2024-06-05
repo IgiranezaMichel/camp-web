2473f0b78e3b18dc87d94b60d877bc720e79c053
IgiranezaMichel <michel.igiraneza@auca.ac.rw>
2024-06-05 17:32:25 +0200

    LoginContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import {Route, BrowserRouter as  Router, Routes } from 'react-router-dom'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { LevelUi } from './pages/user/levels'
 import { BookUi } from './pages/user/books'
 import { Camp } from './pages/user/camp'
@@ -7,26 +8,41 @@ import { Dashboard } from './pages/user/index'
 import { User } from './pages/user/accountHolder'
 import { Logout } from './pages/logout'
 import { Church } from './pages/user/church'
+import { Authentication } from './pages/user/authentication'
+import { LoginContext } from './contexts/loginContext'
+import { SignIn } from './pages/signin'
+import { DataState } from './types/DataState'
+import { useEffect, useState } from 'react'
 function App() {
-
+  const [login,setLogin]=useState<DataState|undefined>(undefined);
+  const data: DataState = {
+    currentState:login,
+    updateState: (data:any) => {setLogin(data) }
+  }
+  useEffect(
+    ()=>{
+      
+    },[login]
+  )
   return (
-    <>
-     <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<BookUi/>} />
-          <Route path="/admin" element={<Dashboard/>} />
-          <Route path="/admin/church" element={<Church/>} />
-          <Route path="/admin/level" element={<LevelUi/>} />
-          <Route path="/admin/book" element={<BookUi/>} />
-          <Route path="/admin/camp" element={<Camp/>} />
-          <Route path="/admin/exams" element={<Exam/>} />
-          <Route path="/admin/user" element={<User/>} />
-          <Route path="/admin/logout" element={<Logout/>} />
-        </Routes>
-      </div>
+    <LoginContext.Provider value={data}>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<BookUi />} />
+            <Route path="/login" element={<SignIn />} />
+            <Route path="/admin" element={<Dashboard />} />
+            <Route path="/admin/church" element={<Church />} />
+            <Route path="/admin/level" element={<Authentication><LevelUi /></Authentication>} />
+            <Route path="/admin/book" element={<Authentication><BookUi /></Authentication>} />
+            <Route path="/admin/camp" element={<Authentication><Camp /></Authentication>} />
+            <Route path="/admin/exams" element={<Authentication><Exam /></Authentication>} />
+            <Route path="/admin/user" element={<User />} />
+            <Route path="/admin/logout" element={<Logout />} />
+          </Routes>
+        </div>
       </Router>
-    </>
+    </LoginContext.Provider>
   )
 }
 
